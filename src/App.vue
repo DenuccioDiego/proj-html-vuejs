026644cdacf2b4975cfb3c6fab55d3165790a857
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-25 11:14:33 +0100

    component declaration

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div>
     
-    <HelloWorld />
+    <header>
+      <Logo />
+      <NavBar />
+    </header>
+    
+
+    <main>
+      <Jumbotron />
+      <SectionDescription />
+      <SectionCardNewCourses />
+
+    </main>
+
+    <footer>
+      <Footer />
+    </footer>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/Logo.vue'
+import NavBar from './components/NavBar.vue'
+import Jumbotron from './components/Jumbotron.vue'
+import SectionDescription from './components/SectionDescription.vue'
+import SectionCardNewCourses from './components/SectionCardNewCourses.vue'
+import Footer from './components/Footer.vue'
+
+
+
+
 
 export default {
   
   components: {
-    HelloWorld
+    Logo,
+    NavBar,
+    Jumbotron,
+    SectionDescription,
+    SectionCardNewCourses,
+    Footer,
+    
   }
 }
 </script>
