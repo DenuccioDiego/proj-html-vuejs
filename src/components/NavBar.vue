2f7f098237ae1f66e4b80451ac1b385953439a68
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-25 11:26:54 +0100

    installation bootstrap

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
+       <div class="row ">
+            <div class="col-2">ciao</div>
+            <div class="col-2">ciao</div>
+            <div class="col-2">ciao</div>
+            <div class="col-2">ciao</div>
+            <div class="col-2">ciao</div>
+            <div class="col-2">ciao</div>
+
+       </div>
+     
      <font-awesome-icon icon="user-secret" />
+
   </div>
 </template>
 
